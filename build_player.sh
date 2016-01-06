138490564c8c6f3fcf90e5c2c3c2e127f11b5456
Anton Grbin <anton.grbin@carnet.hr>
2016-01-06 22:42:17 +0000

    add guessit and subliminal support

--- build_player.sh
+++ build_player.sh
@@ -1,58 +1,95 @@
 #!/bin/bash
-# ./build_player.sh [videofile]
+# ./build_player.sh directory
+# before attaching this script to torrent-finish try running it manually
+# Dependencies:
+#   npm install -g srt-to-vtt
+#   pip install guessit
+#   pip install sublinimal
+SUBLINIMAL_CACHE=/var/downloads/subliminal-cache/
 
-set -e
-echo checking if there is subdb and srt-to-vtt on the system..
-which subdb &> /dev/null
-which srt-to-vtt &> /dev/null
+function main {
+  check_deps
+  run_for_dir "$1"
+}
 
-videopath=$1
-
-# check that videopath has supported extension.
-extension="${videopath##*.}"
-if echo $extension | tr  '[:upper:]' '[:lower:]' | grep -E '(avi|mp4|mkv)' > /dev/null; then
-  echo processing $videopath
-else
-  echo $videopath has no vaild extension
-  exit 1
-fi
-
-srtpath=${videopath%.*}.srt
-vttpath=${videopath%.*}.vtt
-htmlpath=${videopath%.*}.html
-
-relvideo=$(basename $videopath)
-relvtt=$(basename $vttpath)
-
-if [ ! -f $srtpath ]; then
-  # ignore if subtitle cant be downloaded
-  ( subdb download $videopath )
-fi
-
-if [ -f $srtpath ]; then
-  if [ ! -f $vttpath ]; then
-    srt-to-vtt $srtpath > $vttpath
+function run_for_dir {
+  dir="$1"
+  if [ ! -d "$dir" ]; then
+    echo "$dir should be a dir."
+    exit 1
   fi
-fi
+  find "$dir" -iregex '.*\(avi\|mp4\|mkv\)$' -type f | while read videopath; do
+    build_for_movie "$videopath"
+  done
+}
 
-cat > $htmlpath <<EOF
-<!doctype html>
-<html>
-  <head>
-    <style>
-      video { width: 100%; }
-      html { margin: 0px; width: 100%; height: 100%; }
-      body { margin: 0px; }
-    </style>
-  </head>
-  <body>
-    <video controls autoplay id="video">
-      <source src="$relvideo" type="video/mp4" />
-      <track kind="subtitles" src="$relvtt" default />
-      <div>
-        <strong>Sorry, youll need an HTML5 Video capable browser.</strong>
-      </div>
-    </video>
-  </body>
-</html>
+function check_deps {
+  echo -ne checking if there is sublinimal and srt-to-vtt on the system...
+  which subliminal &> /dev/null || exit 1
+  which srt-to-vtt &> /dev/null || exit 1
+  echo ok
+}
+
+# build_for_movie videofile
+# tries to download subtitles for a movie and builds a .html player.
+function build_for_movie {
+  videopath="$1"
+
+  srtpath=${videopath%.*}.srt
+  vttpath=${videopath%.*}.vtt
+  htmlpath=${videopath%.*}.html
+  txtpath=${videopath%.*}.txt
+
+  relvideo=$(basename "$videopath")
+  relvtt=$(basename "$vttpath")
+
+  echo processing $relvideo..
+
+  # extract some information from video
+  guessit "$videopath" > "$txtpath"
+
+  if [ ! -f "$srtpath" ]; then
+    # ignore if subtitle cant be downloaded
+    # throttle download
+    sleep 3
+    echo downloading subtitles..
+    subliminal --cache-dir=$SUBLINIMAL_CACHE \
+      download --force --language=en --single "$videopath" &> /dev/null
+  fi
+
+  if [ -f "$srtpath" ]; then
+    echo subtitles found. converting to vtt..
+    if [ ! -f "$vttpath" ]; then
+      srt-to-vtt "$srtpath" > "$vttpath"
+    fi
+  else
+    echo no subtitles found.
+  fi
+
+  cat > "$htmlpath" <<EOF
+  <!doctype html>
+  <html>
+    <head>
+      <style>
+        video { width: 100%; height: 100%; }
+        html { margin: 0px; }
+        body { margin: 0px; }
+      </style>
+    </head>
+    <body>
+      <video controls autoplay id="video">
+        <source src="$relvideo" type="video/mp4" />
+        <track kind="subtitles" src="$relvtt" default />
+        <div>
+          <strong>Sorry, youll need an HTML5 Video capable browser.</strong>
+        </div>
+      </video>
+    </body>
+  </html>
 EOF
+  echo html created.
+  echo
+  return $retval
+}
+
+main "$1"
